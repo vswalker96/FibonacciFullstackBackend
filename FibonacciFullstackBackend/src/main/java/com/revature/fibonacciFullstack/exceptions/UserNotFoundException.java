46b36570dd9b78dac27b2b85dcf3f2ae1c50f2b6
Victoria Walker <victoriawalker@victorias-mbp>
2020-05-19 11:56:06 -0400

    current version

--- FibonacciFullstackBackend/src/main/java/com/revature/fibonacciFullstack/exceptions/UserNotFoundException.java
+++ FibonacciFullstackBackend/src/main/java/com/revature/fibonacciFullstack/exceptions/UserNotFoundException.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class UserNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public UserNotFoundException(String message) {
 		super(message);
+		// TODO Auto-generated constructor stub
 	}
+	
+
+	
+
+	
 
 }
--- FibonacciFullstackBackend/src/test/java/com/revature/fibonacciFullstack/FibonacciFullstackBackendApplicationTests.java
+++ FibonacciFullstackBackend/src/test/java/com/revature/fibonacciFullstack/FibonacciFullstackBackendApplicationTests.java
@@ -1,13 +1,43 @@
 package com.revature.fibonacciFullstack;
 
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.validation.constraints.AssertTrue;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.revature.fibonacciFullstack.models.FibonacciNumber;
+import com.revature.fibonacciFullstack.services.*;
 
 @SpringBootTest
 class FibonacciFullstackBackendApplicationTests {
+	
+	@Autowired
+	FibonacciSequenceService fibService;
 
+	
+	
 	@Test
 	void contextLoads() {
 	}
+	
+	
+	@Test
+	void testFibonacciSequence() {
+		
+		int actual = fibService.fibonacci(6);
+		assertEquals(8, actual);
 
-}
+	}
+	
+
+
+	
+}	
+
+
+
